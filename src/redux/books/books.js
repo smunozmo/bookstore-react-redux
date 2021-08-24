a9442221f6671680feaafa48cbbaba4bffb5e509
Sebastián M <smunozmo@gmail.com>
2021-08-24 12:02:10 -0500

    Add linters.

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -2,63 +2,63 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Books = () => (
-    <div className="container align-middle py-5">
+  <div className="container align-middle py-5">
     <div className="container my-5 align-middle" id="formContainer">
-        <div className="card">
-            <div className="card-header">
-                <div className="container">
-                    <div className="row">
-                      <div className="col">
-                        Bookstore CMS
-                      </div>
-                      <div className="col">
-                        <Link className="links" to="/">BOOKS</Link>
-                      </div>
-                      <div className="col">
-                        <Link className="links" to="/categories">CATEGORIES</Link>
-                      </div>
-                    </div>
-                </div>
+      <div className="card">
+        <div className="card-header">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                Bookstore CMS
+              </div>
+              <div className="col">
+                <Link className="links" to="/">BOOKS</Link>
+              </div>
+              <div className="col">
+                <Link className="links" to="/categories">CATEGORIES</Link>
+              </div>
             </div>
+          </div>
+        </div>
+        <div className="card-body">
+          <div className="card">
             <div className="card-body">
-                <div className="card">
-                    <div className="card-body">
-                        <div className="container">
-                            <div className="row">
-                              <div className="col">
-                                <p>Action</p>
-                                <p>The Hunger Games</p>
-                                <p>Suzanne Collins</p>
-                                <div className="container">
-                                    <div className="row">
-                                      <div className="col">
-                                        Comments
-                                      </div>
-                                      <div className="col">
-                                        Remove
-                                      </div>
-                                      <div className="col">
-                                        Edit
-                                      </div>
-                                    </div>
-                                </div>
-                              </div>
-                              <div className="col">
-                                64% Completed
-                              </div>
-                              <div className="col">
-                                <p>Current chapter</p>
-                                <p>Chapter 17</p>
-                                <button type="button" className="btn btn-primary">UPDATE PROGRESS</button>
-                              </div>
-                            </div>
-                          </div>
+              <div className="container">
+                <div className="row">
+                  <div className="col">
+                    <p>Action</p>
+                    <p>The Hunger Games</p>
+                    <p>Suzanne Collins</p>
+                    <div className="container">
+                      <div className="row">
+                        <div className="col">
+                          Comments
+                        </div>
+                        <div className="col">
+                          Remove
+                        </div>
+                        <div className="col">
+                          Edit
+                        </div>
+                      </div>
                     </div>
                   </div>
+                  <div className="col">
+                    64% Completed
+                  </div>
+                  <div className="col">
+                    <p>Current chapter</p>
+                    <p>Chapter 17</p>
+                    <button type="button" className="btn btn-primary">UPDATE PROGRESS</button>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 );
 
 export default Books;
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -2,34 +2,34 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Categories = () => (
-    <div className="container align-middle py-5">
+  <div className="container align-middle py-5">
     <div className="container my-5 align-middle" id="formContainer">
-        <div className="card">
-            <div className="card-header">
-                <div className="container">
-                    <div className="row">
-                      <div className="col">
-                        Bookstore CMS
-                      </div>
-                      <div className="col">
-                        <Link className="links" to="/">BOOKS</Link>
-                      </div>
-                      <div className="col">
-                        <Link className="links" to="/categories">CATEGORIES</Link>
-                      </div>
-                    </div>
-                </div>
+      <div className="card">
+        <div className="card-header">
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                Bookstore CMS
+              </div>
+              <div className="col">
+                <Link className="links" to="/">BOOKS</Link>
+              </div>
+              <div className="col">
+                <Link className="links" to="/categories">CATEGORIES</Link>
+              </div>
             </div>
-            <div className="card-body">
-                <div className="card">
-                    <div className="card-body">
-                        Under Construction
-                    </div>
-                  </div>
-            </div>
+          </div>
         </div>
+        <div className="card-body">
+          <div className="card">
+            <div className="card-body">
+              Under Construction
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </div>
 );
 
 export default Categories;
